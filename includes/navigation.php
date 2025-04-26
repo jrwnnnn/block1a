c17a84279adb147c3552cd73a35c710ae66cfc8a
Mark Jerwin <jrwn.xyz@gmail.com>
2025-04-26 12:08:39 +0800

    Refactor rules structure: Consolidate individual rule pages into a dynamic rules.php file, utilizing JSON for content management. Remove obsolete HTML files for enforcement, final provisions, governance structure, footer, preamble, rights and responsibilities, and rules of conduct. Update navigation and footer includes for consistency.

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,5 +1,5 @@
 <nav class="bg-[#1A212B] p-4 px-5 md:px-30 flex items-center justify-between">
-    <img src="..//assets/cs1a.png" alt="logo" class="w-20 hover:cursor-pointer" onclick="window.location.replace('../index.php')">
+    <img src="assets/cs1a.png" alt="logo" class="w-20 hover:cursor-pointer" onclick="window.location.replace('index.php')">
     
     <button id="menu-toggle" class="md:hidden text-white focus:outline-none">
         <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2"
@@ -10,12 +10,12 @@
     </button>
     
     <div id="nav-links" class="hidden md:grid md:grid-cols-7 absolute md:static top-20 left-0 w-full md:w-auto bg-[#1A212B] text-center md:flex-row md:space-x-4 transition-all duration-300 ease-in-out z-10">
-        <a href="../index.php" class="nav-tab block py-2 md:inline">Home</a>
-        <a href="pages/blog.php" class="nav-tab block py-2 md:inline">Blog</a>
-        <a href="pages/preamble.html" class="nav-tab block py-2 md:inline">Rules</a>
-        <a href="pages/bluemap.php" class="nav-tab block py-2 md:inline">BlueMap</a>
-        <a href="../404.php" class="nav-tab block py-2 md:inline">Playpass</a>
-        <a href="../404.php" class="nav-tab block py-2 md:inline">Help and Support</a>
+        <a href="index.php" class="nav-tab block py-2 md:inline">Home</a>
+        <a href="blog.php" class="nav-tab block py-2 md:inline">Blog</a>
+        <a href="rules.php" class="nav-tab block py-2 md:inline">Rules</a>
+        <a href="bluemap.php" class="nav-tab block py-2 md:inline">BlueMap</a>
+        <a href="404.php" class="nav-tab block py-2 md:inline">Playpass</a>
+        <a href="404.php" class="nav-tab block py-2 md:inline">Help and Support</a>
         <a href="pages/signup.php" class="nav-tab block py-2 md:inline">Me</a>
     </div>
 </nav>
