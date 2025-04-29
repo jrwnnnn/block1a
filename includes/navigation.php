b3855258330c3c25cab2a80f141a9c64b4a14e21
Mark Jerwin <jrwn.xyz@gmail.com>
2025-04-29 23:43:12 +0800

    Update footer and navigation links: Correct social media URLs and refactor login/profile logic

--- includes/navigation.php
+++ includes/navigation.php
@@ -1,12 +1,12 @@
 <?php
 
-$grid7 = "Login"; // Default value
+$grid6 = "Login";
 
 if (isset($_SESSION['user_id'])) {
-    $grid7 = "Profile";
-    $grid7_link = "profile.php";
+    $grid6 = "Profile";
+    $grid6_link = "profile.php";
 } else {
-    $grid7_link = "pages/login.php";
+    $grid6_link = "pages/login.php";
 }
 ?>
 
@@ -21,13 +21,12 @@ if (isset($_SESSION['user_id'])) {
         </svg>
     </button>
     
-    <div id="nav-links" class="hidden md:grid md:grid-cols-7 absolute md:static top-20 left-0 w-full md:w-auto bg-[#1A212B] text-center md:flex-row md:space-x-4 transition-all duration-300 ease-in-out z-10">
+    <div id="nav-links" class="hidden md:grid md:grid-cols-6 absolute md:static top-20 left-0 w-full md:w-auto bg-[#1A212B] text-center md:flex-row md:space-x-4 transition-all duration-300 ease-in-out z-10">
         <a href="index.php" class="nav-tab block py-2 md:inline">Home</a>
         <a href="blog.php" class="nav-tab block py-2 md:inline">Blog</a>
         <a href="rules.php" class="nav-tab block py-2 md:inline">Rules</a>
         <a href="bluemap.php" class="nav-tab block py-2 md:inline">BlueMap</a>
-        <a href="404.php" class="nav-tab block py-2 md:inline">Playpass</a>
-        <a href="404.php" class="nav-tab block py-2 md:inline">Help and Support</a>
-        <a href="<?php echo $grid7_link; ?>" class="nav-tab block py-2 md:inline"><?php echo $grid7; ?></a>
+        <a href="help-and-support.php" class="nav-tab block py-2 md:inline">Help and Support</a>
+        <a href="<?php echo $grid6_link; ?>" class="nav-tab block py-2 md:inline"><?php echo $grid6; ?></a>
     </div>
 </nav>
