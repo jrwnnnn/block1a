4ef121a9338433b1fcf5ddaca99703480d9d2745
Mark Jerwin <jrwn.xyz@gmail.com>
2025-05-01 15:41:20 +0800

    fix: update text size in footer for consistency

--- includes/footer.php
+++ includes/footer.php
@@ -5,7 +5,7 @@
         <img src="assets/social_icons/instagram.png" alt="instagram" class="w-10 hover:cursor-pointer" onclick="window.open('https://www.instagram.com/block1a_/')">
     </div>
     <div class="flex flex-col justify-center items-center my-7 md:mx-0 mx-5">
-        <p class="text-sm text-white text-center">© 2020-2025, Block1A Server - All rights reserved.</p>
-        <p class="text-sm text-white text-center">Not affiliated with Mojang, AB. Nor should we be considered a server endorsed by Mojang, AB.</p>
+        <p class="text-xsm text-white text-center">© 2020-2025, Block1A Server - All rights reserved.</p>
+        <p class="text-xsm text-white text-center">Not affiliated with Mojang, AB. Nor should we be considered a server endorsed by Mojang, AB.</p>
     </div>
 </footer>
