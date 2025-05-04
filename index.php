22d6d3aa63c6c4036fe4aaa5cbb34e55d5d0e946
Mark Jerwin <jrwn.xyz@gmail.com>
2025-05-04 19:26:30 +0800

    style: Improve styling consistency across index and news pages

--- index.php
+++ index.php
@@ -28,26 +28,26 @@
   <title>Block1A - Home</title>
 </head>
 <body>
-    <section class="flex flex-col bg-cover bg-center bg-no-repeat min-h-screen" style="background-image: url('assets/home_splash.webp')">
+    <section class="flex flex-col min-h-screen bg-center bg-no-repeat bg-cover" style="background-image: url('assets/home_splash.webp')">
         <?php include 'includes/navigation.php'; ?>
-        <div class="flex flex-col md:items-start md:justify-end justify-center items-center flex-grow text-white pb-20 md:px-30 px-10">
-            <p class="md:text-6xl text-5xl text-center font-bold pb-5 md:pt-0 pt-9">HOP IN, BUILD STUFF, HAVE FUN</p>
-            <p class="md:text-lg text-center">The Official Minecraft Server of BSCS-1A! Available for both Minecraft Java and Bedrock Platform.</p>
+        <div class="flex flex-col items-center justify-center flex-grow px-10 pb-20 text-white md:items-start md:justify-end md:px-30">
+            <p class="pb-5 text-5xl font-bold text-center md:text-6xl md:pt-0 pt-9">HOP IN, BUILD STUFF, HAVE FUN</p>
+            <p class="text-center md:text-lg">The Official Minecraft Server of BSCS-1A! Available for both Minecraft Java and Bedrock Platform.</p>
             <button id="copy-button" onclick="copyToClipboard()" class="bg-yellow-500 text-black md:text-lg font-bold py-2 px-5 rounded-md mt-5 hover:bg-[#2D3748] hover:text-white hover:cursor-pointer transition duration-300 ease-in-out">Copy IP : cs1a.minecra.fr</button>
         </div>        
     </section>
     <section class="bg-[#1a202a] md:px-30 px-5 py-5">
-        <div class="grid grid-cols-3 md:gap-10 gap-3">
-            <div class="text-white text-center">
-                <p class="text-yellow-500 md:text-5xl text-2xl font-bold">27</p>
+        <div class="grid grid-cols-3 gap-3 md:gap-10">
+            <div class="text-center text-white">
+                <p class="text-2xl font-bold text-yellow-500 md:text-5xl">27</p>
                 <p>Unique players</p>
             </div>
-            <div class="text-white text-center">
-                <p id="player-count" class="text-yellow-500 md:text-5xl text-2xl font-bold">Loading...</p>
+            <div class="text-center text-white">
+                <p id="player-count" class="text-2xl font-bold text-yellow-500 md:text-5xl">Loading...</p>
                 <p>Online players</p>
             </div>
-            <div class="text-white text-center">
-                <p class="text-yellow-500 md:text-5xl text-2xl font-bold">99.8%</p>
+            <div class="text-center text-white">
+                <p class="text-2xl font-bold text-yellow-500 md:text-5xl">99.8%</p>
                 <p>Server Uptime</p>
             </div>
         </div>
@@ -56,18 +56,18 @@
         <?php if ($spotlightPost): ?>
             <div class="relative">
                 <img src="<?= htmlspecialchars($spotlightPost['cover']) ?>" class="w-full md:h-[70vh] h-[80vh] object-cover object-center brightness-75">
-                <div class="absolute inset-0 flex flex-col md:justify-center justify-end items-start md:px-30 px-5 py-10">
-                    <p class="text-blue-400 text-lg tracking-widest">Spotlight</p>
-                    <p class="md:text-5xl text-3xl font-bold pb-5"><?= htmlspecialchars($spotlightPost['title']) ?></p>
-                    <p class="md:text-lg text-base"><?= htmlspecialchars($spotlightPost['subtitle']) ?></p>
-                    <button id="copy-button" onclick="window.location.href='news/article.php?id=<?= $spotlightPost['id'] ?>'" class="bg-blue-500 text-white md:text-lg font-bold py-2 px-5 rounded-md mt-5 hover:bg-white hover:text-black hover:cursor-pointer transition duration-300 ease-in-out">Read</button>
+                <div class="absolute inset-0 flex flex-col items-start justify-end px-5 py-10 md:justify-center md:px-30">
+                    <p class="text-lg tracking-widest text-blue-400">Spotlight</p>
+                    <p class="pb-5 text-3xl font-bold md:text-5xl"><?= htmlspecialchars($spotlightPost['title']) ?></p>
+                    <p class="text-base md:text-lg"><?= htmlspecialchars($spotlightPost['subtitle']) ?></p>
+                    <button id="copy-button" onclick="window.location.href='news/article.php?id=<?= $spotlightPost['id'] ?>'" class="px-5 py-2 mt-5 font-bold text-white transition duration-300 ease-in-out bg-blue-500 rounded-md md:text-lg hover:bg-white hover:text-black hover:cursor-pointer">Read</button>
                 </div>
             </div>
         <?php endif; ?>
     </section>
-    <section class="flex flex-col bg-white md:px-30 px-5 py-10">
-        <p class="text-black md:text-5xl text-3xl font-bold mb-7">News</p>
-        <div class="grid md:grid-cols-3 gap-10 hover:cursor-pointer">
+    <section class="flex flex-col px-5 py-10 bg-white md:px-30">
+        <p class="text-3xl font-bold text-black md:text-5xl mb-7">News</p>
+        <div class="grid gap-10 md:grid-cols-3 hover:cursor-pointer">
             <?php foreach ($nonSpotlightPosts  as $post): ?>
                 <?php
                     $tagColor = match ($post['tag']) {
@@ -78,49 +78,49 @@
                         default => 'text-white',
                     };
                 ?>
-                <div onclick="window.location.href='news/article.php?id=<?= $post['id'] ?>'" class="hover:cursor-pointer text-black">
-                    <div class="aspect-video w-full overflow-hidden rounded-md mb-4">
-                        <img src="<?= htmlspecialchars($post['cover']) ?>" class="h-full w-full object-cover transition ease-in-out duration-500 hover:scale-105">
+                <div onclick="window.location.href='news/article.php?id=<?= $post['id'] ?>'" class="text-black hover:cursor-pointer">
+                    <div class="w-full mb-4 overflow-hidden rounded-md aspect-video">
+                        <img src="<?= htmlspecialchars($post['cover']) ?>" class="object-cover w-full h-full transition duration-500 ease-in-out hover:scale-105">
                     </div>
                     <p class="<?= $tagColor ?> capitalize"><?= htmlspecialchars(str_replace('_', ' ', $post['tag'])) ?></p>
-                    <p class="text-2xl font-bold mb-2"><?= htmlspecialchars($post['title']) ?></p>
+                    <p class="mb-2 text-2xl font-bold"><?= htmlspecialchars($post['title']) ?></p>
                     <p><?= htmlspecialchars($post['subtitle']) ?></p>
-                    <div class="flex items-center gap-2 mt-3">                      
-                        <p class="text-gray-600 text-sm"><?= date("F d, Y", strtotime($post['date_posted'])) ?></p>
-                        <hr class="md:hidden border-gray-500 flex-grow border-1">
+                    <div class="flex items-center gap-2 mt-5">                      
+                        <p class="text-sm text-gray-600"><?= date("F d, Y", strtotime($post['date_posted'])) ?></p>
+                        <hr class="flex-grow border-gray-500 md:hidden border-1">
                     </div>
                 </div>
             <?php endforeach; ?>
         </div>
-        <div class="mt-10 flex justify-end items-center gap-2">
-            <a href="news.php" class="text-blue-500 hover:text-blue-700 tracking-widest flex items-center">
+        <div class="flex items-center justify-end gap-2 mt-10">
+            <a href="news.php" class="flex items-center tracking-widest text-blue-500 hover:text-blue-700">
             See all news
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+            <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 ml-1" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
             </svg>
             </a>
         </div>
     </section>
     <section class="flex flex-col bg-[#2D3748] md:px-30 px-5 py-10">
-        <p class="md:text-6xl text-4xl text-yellow-500 font-bold mb-7">The Server</p>
+        <p class="text-4xl font-bold text-yellow-500 md:text-6xl mb-7">The Server</p>
         <div class="md:relative overflow-hidden rounded-md w-full md:h-[80vh] h-full group">
           <div id="carousel" class="flex transition-transform duration-500 ease-in-out group-hover:brightness-50">
-            <img src="assets/carousel-1.webp" alt="Screenshot 1" class="w-full flex-shrink-0">
-            <img src="assets/carousel-2.webp" alt="Screenshot 2" class="w-full flex-shrink-0">
-            <img src="assets/carousel-3.webp" alt="Screenshot 3" class="w-full flex-shrink-0">
-            <img src="assets/carousel-4.webp" alt="Screenshot 4" class="w-full flex-shrink-0">
-            <img src="assets/carousel-5.webp" alt="Screenshot 5" class="w-full flex-shrink-0">
-            <img src="assets/carousel-6.webp" alt="Screenshot 6" class="w-full flex-shrink-0">
-            <img src="assets/carousel-7.webp" alt="Screenshot 7" class="w-full flex-shrink-0">
+            <img src="assets/carousel-1.webp" alt="Screenshot 1" class="flex-shrink-0 w-full">
+            <img src="assets/carousel-2.webp" alt="Screenshot 2" class="flex-shrink-0 w-full">
+            <img src="assets/carousel-3.webp" alt="Screenshot 3" class="flex-shrink-0 w-full">
+            <img src="assets/carousel-4.webp" alt="Screenshot 4" class="flex-shrink-0 w-full">
+            <img src="assets/carousel-5.webp" alt="Screenshot 5" class="flex-shrink-0 w-full">
+            <img src="assets/carousel-6.webp" alt="Screenshot 6" class="flex-shrink-0 w-full">
+            <img src="assets/carousel-7.webp" alt="Screenshot 7" class="flex-shrink-0 w-full">
           </div>
-          <div class="md:absolute md:inset-0 flex flex-col gap-5 items-center justify-center md:opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
-            <img src="assets/cs1a.png" class="md:block hidden w-50">
-            <p class="text-white md:text-lg md:text-center md:mt-0 mt-5 md:px-50">This server kicked off on December 10, 2024, right before Christmas break. It started as a chill place for just 7 of us, playing for fun on Aternos. Since then, things have grown — we’ve moved to a premium server for smoother gameplay and more cool stuff to do. It’s still the same cozy vibe, just better performance and more space to hang out.</p>
+          <div class="flex flex-col items-center justify-center gap-5 transition-opacity duration-300 ease-in-out md:absolute md:inset-0 md:opacity-0 group-hover:opacity-100">
+            <img src="assets/cs1a.png" class="hidden md:block w-50">
+            <p class="mt-5 text-white md:text-lg md:text-center md:mt-0 md:px-50">This server kicked off on December 10, 2024, right before Christmas break. It started as a chill place for just 7 of us, playing for fun on Aternos. Since then, things have grown — we’ve moved to a premium server for smoother gameplay and more cool stuff to do. It’s still the same cozy vibe, just better performance and more space to hang out.</p>
           </div>
         </div>
     </section>
-    <section class="flex flex-col items-center bg-blue-500 md:px-30 px-5 py-5">
-        <p class=" text-white text-center"> Whether you’re here to build, explore, or just vibe with friends, welcome to the crew!</p>
+    <section class="flex flex-col items-center px-5 py-5 bg-blue-500 md:px-30">
+        <p class="text-center text-white "> Whether you’re here to build, explore, or just vibe with friends, welcome to the crew!</p>
     </section>
     <?php include 'includes/footer.php'; ?>
     <script src="script/index.js"></script>
--- news/editor.php
+++ news/editor.php
@@ -41,8 +41,8 @@
 </head>
 <body class="min-h-screen">
     <?php include 'includes/navigation.php'; ?>
-    <!-- <img src="" id="coverPreview" alt="cover" class="w-full max-h-[40vh] object-cover object-center"> -->
-    <div id="loading" class="invisible fixed inset-0 flex flex-col items-center justify-center z-99" style="background-color: rgba(0, 0, 0, 0.75);">
+    <img src="" id="coverPreview" alt="cover" class="hidden w-full max-h-[40vh] object-cover object-center">
+    <div id="loading" class="fixed inset-0 flex flex-col items-center justify-center invisible z-99" style="background-color: rgba(0, 0, 0, 0.75);">
         <img src="../assets/panda-roll.gif" alt="Loading" class="w-30 h-30">
     </div>
     <section class="flex flex-col md:px-30 px-5 py-10 pb-20 text-white bg-[#2D3748]">
@@ -50,32 +50,32 @@
             <input type="hidden" name="action" value="<?= $action ?>">
             <input type="hidden" name="id" value="<?= $article ? $article['id'] : '' ?>">
 
-            <input type="text" name="title" placeholder="Title" class="text-white md:text-6xl text-4xl font-bold w-full focus:outline-none" value="<?= $article ? htmlspecialchars($article['title']) : '' ?>" required autocomplete="off">
-            <textarea type="text" name="subtitle" placeholder="Subtitle" class="text-white md:text-2xl text-lg w-full focus:outline-none" required autocomplete="off"><?= $article ? htmlspecialchars($article['subtitle']) : '' ?></textarea >
+            <input type="text" name="title" placeholder="Title" class="w-full text-4xl font-bold text-white md:text-6xl focus:outline-none" value="<?= $article ? htmlspecialchars($article['title']) : '' ?>" required autocomplete="off">
+            <textarea type="text" name="subtitle" placeholder="Subtitle" class="w-full text-lg text-white md:text-2xl focus:outline-none" required autocomplete="off"><?= $article ? htmlspecialchars($article['subtitle']) : '' ?></textarea >
 
-            <div class="flex md:flex-row flex-col gap-3">
-                <input type="text" name="cover" placeholder="Cover Image URL" class="bg-gray-800 px-3 py-2 rounded-lg focus:outline-none text-white" value="<?= $article ? htmlspecialchars($article['cover']) : '' ?>" required autocomplete="off">
+            <div class="flex flex-col gap-3 md:flex-row">
+                <input type="text" name="cover" placeholder="Cover Image URL" class="px-3 py-2 text-white bg-gray-800 rounded-lg focus:outline-none" value="<?= $article ? htmlspecialchars($article['cover']) : '' ?>" required autocomplete="off">
                 <div class="flex gap-3">
-                    <select name="tag" class="md:w-auto md:order flex-grow bg-gray-800 px-3 py-2 rounded-lg focus:outline-none text-white">
+                    <select name="tag" class="flex-grow px-3 py-2 text-white bg-gray-800 rounded-lg md:w-auto md:order focus:outline-none">
                         <option value="server_updates" <?= $article && $article['tag'] == 'server_updates' ? 'selected' : '' ?>>Server Updates</option>
                         <option value="event" <?= $article && $article['tag'] == 'event' ? 'selected' : '' ?>>Event</option>
                         <option value="game_updates" <?= $article && $article['tag'] == 'game_updates' ? 'selected' : '' ?>>Game Updates</option>
                         <option value="tech" <?= $article && $article['tag'] == 'tech' ? 'selected' : '' ?>>Tech</option>
                     </select>
                     <div class="flex gap-3 px-3 py-2 bg-gray-800 rounded-md">
-                        <img src="https://mc-heads.net/avatar/<?php echo htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8');?>" alt="avatar" class="w-5 aspect-square rounded">
+                        <img src="https://mc-heads.net/avatar/<?php echo htmlspecialchars($_SESSION['username'], ENT_QUOTES, 'UTF-8');?>" alt="avatar" class="w-5 rounded aspect-square">
                         <p class="text-white"><?= $_SESSION['username'] ?></p>
                     </div>
                 </div>
             </div>
             <textarea name="content" id="editor"><?= $article ? htmlspecialchars($article['content']) : '' ?></textarea>
             <div class="flex items-start justify-between gap-3">
-                <button type="submit" class="bg-yellow-500 text-black md:text-lg font-bold py-2 px-5 rounded-md hover:bg-yellow-300 hover:cursor-pointer transition duration-300 ease-in-out" onclick="loadingLong()">
+                <button type="submit" class="px-5 py-2 font-bold text-black transition duration-300 ease-in-out bg-yellow-500 rounded-md md:text-lg hover:bg-yellow-300 hover:cursor-pointer" onclick="loadingLong()">
                     <?= $action == 'edit' ? 'Update Article' : 'Post Article' ?>
                 </button>
 
                 <label for="spotlight" class="flex items-center gap-2 text-white">
-                    <input type="checkbox" name="spotlight" id="spotlight" class="w-4 h-4 text-blue-500 focus:ring focus:ring-blue-300 rounded" <?= $article && $article['spotlight'] == 1 ? 'checked' : '' ?>>Spotlight
+                    <input type="checkbox" name="spotlight" id="spotlight" class="w-4 h-4 text-blue-500 rounded focus:ring focus:ring-blue-300" <?= $article && $article['spotlight'] == 1 ? 'checked' : '' ?>>Spotlight
                 </label>
             </div>
         </form>
@@ -84,5 +84,28 @@
     <script src="https://cdn.jsdelivr.net/simplemde/latest/simplemde.min.js"></script>
     <script src="../script/simplemde.js"></script>
     <script src="../script/editor.js"></script>
+    <script>
+    document.addEventListener("DOMContentLoaded", () => {
+        const coverInput = document.querySelector("input[name='cover']");
+        const coverPreview = document.getElementById("coverPreview");
+
+        const updatePreview = () => {
+            const url = coverInput.value.trim();
+            if (url) {
+                coverPreview.src = url;
+                coverPreview.classList.remove("hidden");
+            } else {
+                coverPreview.classList.add("hidden");
+                coverPreview.src = ""; // clear the image
+            }
+        };
+
+        coverInput.addEventListener("input", updatePreview);
+
+        // Set initial state on load (for edit mode)
+        updatePreview();
+    });
+</script>
+
 </body>
 </html>
