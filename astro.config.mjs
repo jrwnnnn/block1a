f37ab7a3c8ae3a8091b8df555d8b21f099f424cc
Mark Jerwin <git@jrwnnnn.me>
2026-09-08 15:06:25 +0800

    chore: Configure Astro for Cloudflare Workers

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	site: "https://jrwnnnn.me",
+	output: "server",
 	vite: {
-		plugins: [tailwindcss()],
+		plugins: [cloudflare(), tailwindcss()],
 	},
 });
