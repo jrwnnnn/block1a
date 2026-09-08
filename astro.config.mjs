681baf15bc19b86e99504921921afc97e6337530
Mark Jerwin <git@jrwnnnn.me>
2026-09-08 15:15:24 +0800

    chore: Configure Astro for Cloudflare Workers (#5)
    * chore: Configure Astro for Cloudflare Workers
    
    * Trigger a preview deployment
    
    * chore: Set Cloudflare as adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
 	site: "https://jrwnnnn.me",
+	output: "server",
+	adapter: cloudflare({
+		remoteBindings: !process.env.CI,
+	}),
 	vite: {
 		plugins: [tailwindcss()],
 	},
