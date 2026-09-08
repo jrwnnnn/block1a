765a10ffadfb4f4c1099e9e259670ea44ece7c7a
Mark Jerwin <git@jrwnnnn.me>
2026-09-08 14:55:28 +0800

    Astro.js rewrite (#4)

--- config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$baseUrl = (strpos($_SERVER['HTTP_HOST'], 'localhost') !== false) 
-    ? 'http://localhost/priv-block1a/' 
-    : 'https://block1a.onrender.com/';
-
-date_default_timezone_set('Asia/Manila');
-?>
